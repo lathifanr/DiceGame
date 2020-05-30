29552eac85fdf20db325c9396bcc9c2f45a11047
Lathifa Ramdhania <lathifanur14@gmail.com>
2020-05-30 20:07:43 +0200

    first init

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var imageSource1 = "images/dice" +randomNumber1+ ".png";
+document.querySelector(".img1").setAttribute("src", imageSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var imageSource2 = "images/dice" +randomNumber2+ ".png";
+document.querySelector(".img2").setAttribute("src", imageSource2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML ="Player 1 win";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 win";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
